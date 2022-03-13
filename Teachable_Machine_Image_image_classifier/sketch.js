bdb07a87645d8b3d32dea333722baef5362def42
김종현|Jonghyun Kim <jhkim@dit.ac.kr>
2022-03-13 18:52:35 +0900

    add pics

--- Teachable_Machine_Image_image_classifier/sketch.js
+++ Teachable_Machine_Image_image_classifier/sketch.js
@@ -1,7 +1,9 @@
 let video;
 let classifier;
-let modelURL = './model/';
+// let modelURL = './model/';
+let modelURL = "https://teachablemachine.withgoogle.com/models/oud6RkwWB/"
 let label = "waiting...";
+let confi = 0;
 
 // STEP 1: Load the model!
 function preload() {
@@ -34,6 +36,7 @@ function draw() {
   textAlign(CENTER, CENTER);
   fill(255);
   text(label, width / 2, height - 16);
+  text(confi, width - 50, height - 16)
 }
 
 
@@ -46,6 +49,9 @@ function gotResults(error, results) {
   }
   // Store the label and classify again!
   label = results[0].label;
+  confi = results[0].confidence
+  confi = confi.toFixed(2); // 소수점 2자리 반올림
   console.log(results[0].label);
+  console.log(confi);
   classifyVideo();
 }
